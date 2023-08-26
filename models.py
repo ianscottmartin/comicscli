f92a9ebf30e1e51d118356d31cc831f526767570
Ian Scott Martin <ianscottmartin@gmail.com>
2023-08-26 14:51:47 -0600

    createing users

--- models.py
+++ models.py
@@ -1,4 +1,28 @@
+from sqlalchemy.orm import declarative_base
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base
 
+class User(Base):
+    __tablename__ = "users"
+    id = Column("user_id", Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    username = Column("user_name", String)
+    email = Column(String(55))
+
+    from faker import Faker
+
+    fake = Faker()
+
+    def __repr__(self):
+        return (
+            f"\n<User"
+            + f"id={self.id}, "
+            + f"username={self.username}, "
+            + f"email={self.email}, "
+            + f"first_name={self.first_name},"
+            + f"last_name={self.last_name},"
+            + ">"
+        )
